b34b42b15579cfb30bf6718508778287805663d2
Couvelard Mathieu <couvelard.mathieu@gmail.com>
2018-07-30 16:26:31 +0200

    Ajout CollectionView

---
+++ Vues/MenuCell.swift
@@ -0,0 +1,13 @@
+//
+//  MenuCell.swift
+//  MonRestoDuMonde
+//
+//  Created by Couvelard Mathieu on 30/07/2018.
+//  Copyright © 2018 Couvelard Mathieu. All rights reserved.
+//
+
+import UIKit
+
+class MenuCell: UICollectionViewCell {
+    
+}
