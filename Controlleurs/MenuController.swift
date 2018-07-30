c12630135a104bacc6929c49789e8f62bfc8001e
Couvelard Mathieu <couvelard.mathieu@gmail.com>
2018-07-30 15:51:31 +0200

    Ajout des controlleurs principaux

---
+++ Controlleurs/MenuController.swift
@@ -0,0 +1,18 @@
+//
+//  MenuController.swift
+//  MonRestoDuMonde
+//
+//  Created by Couvelard Mathieu on 30/07/2018.
+//  Copyright © 2018 Couvelard Mathieu. All rights reserved.
+//
+
+import UIKit
+
+class MenuController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
