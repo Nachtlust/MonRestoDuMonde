20975878c759919439adae764b764f0fe9cf1081
Couvelard Mathieu <couvelard.mathieu@gmail.com>
2018-07-30 16:03:07 +0200

    TabBarCustom

---
+++ Controlleurs/MonTabBar.swift
@@ -0,0 +1,18 @@
+//
+//  MonTabBar.swift
+//  MonRestoDuMonde
+//
+//  Created by Couvelard Mathieu on 30/07/2018.
+//  Copyright © 2018 Couvelard Mathieu. All rights reserved.
+//
+
+import UIKit
+
+class MonTabBar: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.barTintColor = GRIS_TRES_FONCE
+        tabBar.tintColor = GRIS_TRES_CLAIR
+    }
+}
---
+++ utilitaires/Constantes.swift
@@ -0,0 +1,14 @@
+//
+//  Constantes.swift
+//  MonRestoDuMonde
+//
+//  Created by Couvelard Mathieu on 30/07/2018.
+//  Copyright © 2018 Couvelard Mathieu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let NOIR = UIColor.black
+let GRIS_TRES_FONCE = UIColor(red: 30 / 255, green: 30 / 255, blue: 30 / 255, alpha: 1)
+let GRIS_TRES_CLAIR = UIColor(red: 240 / 255, green: 240 / 255, blue: 240 / 255, alpha: 1)
