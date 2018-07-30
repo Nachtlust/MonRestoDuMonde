ec8c991de4c5b3597a6c21cf329b34eb13036687
Couvelard Mathieu <couvelard.mathieu@gmail.com>
2018-07-30 16:33:50 +0200

    Classe Plat

---
+++ Modeles/Enums.swift
@@ -0,0 +1,15 @@
+//
+//  Enums.swift
+//  MonRestoDuMonde
+//
+//  Created by Couvelard Mathieu on 30/07/2018.
+//  Copyright © 2018 Couvelard Mathieu. All rights reserved.
+//
+
+import Foundation
+
+enum Type {
+    case entree
+    case platPrincipal
+    case dessert
+}
