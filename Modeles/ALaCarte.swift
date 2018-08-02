394fa105a3639b3a9cce2a490bfe864d5f75f9a0
Couvelard Mathieu <couvelard.mathieu@gmail.com>
2018-08-02 11:10:45 +0200

    ALaCarte modèle et début controller

---
+++ Modeles/ALaCarte.swift
@@ -0,0 +1,23 @@
+//
+//  ALaCarte.swift
+//  MonRestoDuMonde
+//
+//  Created by Couvelard Mathieu on 02/08/2018.
+//  Copyright © 2018 Couvelard Mathieu. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ALaCarte {
+    private var _type: Type
+    private var _plats: [Plat]
+    
+    var type: Type { return _type }
+    var plats: [Plat] { return _plats }
+    
+    init(type: Type){
+        self._type = type
+        self._plats = [Plat]()
+    }
+}
